f3bbe8fa90d5861a2e242b4e5b933fb70312a50e
D0ANT <70179241+D0ANT@users.noreply.github.com>
2020-10-14 08:37:10 +0900

    mention

--- dcbot.py
+++ dcbot.py
@@ -6,6 +6,10 @@ import asyncio
 import os
 chat = 1
 access_token=os.environ['BOT_TOKEN']
+MJid=os.environ['MJid']
+MINGid=os.environ['MINGid']
+ANOid=os.environ['ANOid']
+DOANTid=os.environ['DOANTid']
 token = access_token # 아까 메모해 둔 토큰을 입력합니다
 client = discord.Client()
 @client.event
@@ -144,50 +148,50 @@ async def on_message(message): # 메시지가 들어 올 때마다 가동되는 
         #채팅/ 맨션
 
         if message.content == "도개미야":
-            await message.channel.send("@도개미#9343")    
+            await message.channel.send(DOANTid)    
         if message.content == "개미야":
-            await message.channel.send("@도개미#9343")   
+            await message.channel.send(DOANTid)   
         if message.content == "도개미":
-            await message.channel.send("@도개미#9343")     
+            await message.channel.send(DOANTid)     
         if message.content == "도개미씨":
-            await message.channel.send("@도개미#9343")
+            await message.channel.send(DOANTid)
         if message.content == "개미씨":
-            await message.channel.send("@도개미#9343")                          
+            await message.channel.send(DOANTid)                          
 
         if message.content == "아노야":
-            await message.channel.send("@트롤아노#0114")
+            await message.channel.send(ANOid)
         if message.content == "아노":
-            await message.channel.send("@트롤아노#0114") 
+            await message.channel.send(ANOid) 
         if message.content == "아노씨":
-            await message.channel.send("@트롤아노#0114")    
+            await message.channel.send(ANOid)    
 
         if message.content == "엠제이야":
-            await message.channel.send("@엠제이#0285")
+            await message.channel.send(MJid)
         if message.content == "엠제이":
-            await message.channel.send("@엠제이#0285")  
+            await message.channel.send(MJid)  
         if message.content == "앰제이야":
-            await message.channel.send("@엠제이#0285")  
+            await message.channel.send(MJid)  
         if message.content == "앰제이":
-            await message.channel.send("@엠제이#0285")   
+            await message.channel.send(MJid)   
         if message.content == "제이야":
-            await message.channel.send("@엠제이#0285")
+            await message.channel.send(MJid)
         if message.content == "앰제이씨":
-            await message.channel.send("@엠제이#0285")
+            await message.channel.send(MJid)
         if message.content == "엠제이씨":
-            await message.channel.send("@엠제이#0285")
+            await message.channel.send(MJid)
         if message.content == "제이씨":
-            await message.channel.send("@엠제이#0285")            
+            await message.channel.send(MJid)            
 
         if message.content == "밍찡아":
-            await message.channel.send("@밍밍찡이#8472")
+            await message.channel.send(MINGid)
         if message.content == "밍밍찡아":
-            await message.channel.send("@밍밍찡이#8472")
+            await message.channel.send(MINGid)
         if message.content == "밍밍찡":
-            await message.channel.send("@밍밍찡이#8472")      
+            await message.channel.send(MINGid)      
         if message.content == "밍밍찡씨":
-            await message.channel.send("@밍밍찡이#8472")             
+            await message.channel.send(MINGid)             
         if message.content == "밍찡씨":
-            await message.channel.send("@밍밍찡이#8472")   
+            await message.channel.send(MINGid)   
 
 
 
