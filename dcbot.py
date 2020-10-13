250e5a2dcbc220f4fb8e0b9dd0322a93e81925c4
D0ANT <70179241+D0ANT@users.noreply.github.com>
2020-10-14 08:42:04 +0900

    Update dcbot.py

--- dcbot.py
+++ dcbot.py
@@ -1,6 +1,4 @@
-# -*- coding:utf-8 -*- 
-# 위에 구문은 # 빼버리시면 문제 생깁니다.
-# 가끔가다 애가 인코딩을 잘못 읽어서 오류를 냅니다. 그것을 대비하기 위해 'utf-8'으로 읽으라고 선언합니다.
+# -*- coding:utf-8 -*-
 import discord
 import asyncio
 import os
@@ -10,16 +8,15 @@ MJid=os.environ['MJid']
 MINGid=os.environ['MINGid']
 ANOid=os.environ['ANOid']
 DOANTid=os.environ['DOANTid']
-token = access_token # 아까 메모해 둔 토큰을 입력합니다
+token = access_token
 client = discord.Client()
 @client.event
 async def on_ready(): # 봇이 준비가 되면 1회 실행되는 부분입니다.
-# 봇이 "반갑습니다"를 플레이 하게 됩니다.
-# 눈치 채셨을지 모르곘지만, discord.Status.online에서 online을 dnd로 바꾸면 "다른 용무 중", idle로 바꾸면 "자리 비움"으로 바뀝니다.
+# discord.Status.online에서 online을 dnd로 바꾸면 "다른 용무 중", idle로 바꾸면 "자리 비움"으로 바뀝니다.
     await client.change_presence(status=discord.Status.online, activity=discord.Game("Minecraft"))
-    print("I'm Ready!") # I'm Ready! 문구를 출력합니다.
-    print(client.user.name) # 봇의 이름을 출력합니다.
-    print(client.user.id) # 봇의 Discord 고유 ID를 출력합니다.
+    print("I'm Ready!")
+    print(client.user.name)
+    print(client.user.id)
 @client.event
 async def on_message(message): # 메시지가 들어 올 때마다 가동되는 구문입니다.
     global chat
@@ -27,10 +24,8 @@ async def on_message(message): # 메시지가 들어 올 때마다 가동되는 
     if message.author.bot: # 채팅을 친 사람이 봇일 경우
         return None # 반응하지 않고 구문을 종료합니다.
     
-    if message.content == "/명령어": # !명령어 라는 채팅을 친다면
-        # 메시지 전송이 두가지 방법이 있습니다. 상황에 맞는 구문을 사용하시면 됩니다.
+    if message.content == "/명령어": # !명령어 라는 채팅을 친다
         # 이 구문은 메시지가 보내진 채널에 메시지를 보내는 구문입니다.  await message.channel.send("")
-        
         # 이 아래 구문은 메시지를 보낸 사람의 DM으로 메시지를 보냅니다.
         await message.author.send("/법")
         await message.author.send("/명령어")
@@ -134,7 +129,7 @@ async def on_message(message): # 메시지가 들어 올 때마다 가동되는 
             await message.channel.send("야생중")
 
         if message.content == "서버 열어":
-            await message.channel.send("싫음 ^^")
+            await message.channel.send("싫음 ^^" #made by D0ANT)
 
         if message.content == "아무나 나랑 채팅할사람?":
             await message.channel.send("ㅇㅇ 나랑")    
